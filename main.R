61f530538d3497dec99857d2762dc3c30fddae41
Andrii Iakovliev <whimsial@users.noreply.github.com>
2021-03-03 14:56:35 +0000

    Update main.R

--- main.R
+++ main.R
@@ -1,5 +1,8 @@
 library(data.table)
+## library(genoscores) -- here we use Genoscores pachage downloaded and installed from public git repo
 
+## here we pull the publically available data (see postBuild for details)
 raw <- fread("trans-eQTLS.txt", header = TRUE)
 
+## do something with using genoscores functions
 head(raw)
