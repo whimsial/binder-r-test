1520516791273b2e2c888dcf4f5ff480c771277a
Andrii Iakovliev <whimsial@users.noreply.github.com>
2021-03-03 14:53:57 +0000

    Update main.R

--- main.R
+++ main.R
@@ -1,10 +1,5 @@
 library(data.table)
 
-bim <- data.table::fread(text=
-"snp,chrom,pos,eff.allele,oth.allele
-rs546,1,93151989,T,
-rs549,1,15220329,A,G
-rs665,1,23854551,C,T
-rs751,1,87392286,A,G")
+raw <- fread("trans-eQTLS.txt", header = TRUE)
 
-print(bim)
+head(raw)
